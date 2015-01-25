46a7a929660011fb4532a5d1f03462ed66bf2d4e
Dave Daniels <davedaniels01@gmail.com>
2015-01-25 15:39:49 -0500

    update8

--- run_analysis.R
+++ run_analysis.R
@@ -9,7 +9,7 @@
 #   5 From the data set in step 4, creates a second, independent tidy data set with 
 #		the average of each variable for each activity and each subject.
 #   
-#   See readme.txt for details
+#   See README.md for details
 
 
 
